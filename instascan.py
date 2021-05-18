d610430bdbeb5f7568c3d532b3f28eddc7d26c2d
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2021-05-18 18:53:24 +0300

    Implemented user posts extraction (instauser module)

--- instascan.py
+++ instascan.py
@@ -1,11 +1,13 @@
-import networking
+from networking import Session
 from instauser import ActiveUser
 
 
 def get_user_keywords(username):
-    session_id = networking.get_session_id()
-    user = ActiveUser(username, session_id)
+    session = Session()
+    user = ActiveUser(username, session.get_session_id())
     user_posts = user.get_user_posts()
+    session.end_session()
+
     return convert_to_keywords_dict(user_posts)
 
 
--- instauser.py
+++ instauser.py
@@ -2,18 +2,43 @@ import instagramy
 
 
 class ActiveUser:
-    def __init__(self, session_id, username):
+    def __init__(self, username, session_id):
         self.session_id = session_id
         self.username = username
-        self.user = instagramy.InstagramUser(username, sessionid=session_id)
+        self.user = InstagramUserExtended(username=username, sessionid=session_id)
 
     def get_user_posts(self):
-        # TODO
-        # print(user.user_data['edge_owner_to_timeline_media']['edges'][0]['node']['edge_media_to_caption']['edges'][0]['node']['text'])
+        return self.get_user_posts()
 
-        #
-        # for post in user.posts:
-        #     print(post)
-        #
 
-        pass
+class InstagramUserExtended(instagramy.InstagramUser):
+    @instagramy.InstagramUser.posts.getter
+    def posts(self) -> list:
+        # TODO override this to unite with get_posts_texts property
+        return super().posts
+        # print("Hello from overrided property!")
+        # posts_tuple_lists = super().posts
+        # posts_details = self.user_data["edge_owner_to_timeline_media"]["edges"]
+        #
+        # for post, i in zip(posts_lists, posts_details):
+        #     post['description'] = i['node']['edge_media_to_caption']['edges'][0]['node']['text']
+        #
+        # return posts_lists
+
+    @property
+    def posts_texts(self) -> list:
+        texts_list = []
+        posts_list = self.user_data["edge_owner_to_timeline_media"]["edges"]
+        for post in posts_list:
+            data = {}
+            try:
+                data["display_url"] = post["node"]["display_url"]
+            except (KeyError, TypeError):
+                data["display_url"] = None
+            try:
+                data["text"] = post['node']['edge_media_to_caption']['edges'][0]['node']['text']
+            except (KeyError, TypeError):
+                data["text"] = None
+            texts_list.append(data)
+
+        return texts_list
