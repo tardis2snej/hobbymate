d5204a181ae80fab81d6d5568778f8045e400f67
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2021-05-18 18:52:10 +0300

    Network module structure update (still placeholders)

--- networking.py
+++ networking.py
@@ -1,5 +1,16 @@
-def get_session_id():
-    # TODO
-    # session_id = "48038714228%3AtcWaPPlddntSLf%3A23"
-    pass
+class Session:
+    def __init__(self):
+        # TODO proper session creation!
+        self.session_id = "48038714228%3AtcWaPPlddntSLf%3A23"
+
+    def start_session(self):
+        # TODO
+        pass
+
+    def get_session_id(self):
+        return self.session_id
+
+    def end_session(self):
+        # TODO
+        pass
 
