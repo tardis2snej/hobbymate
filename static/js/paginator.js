2faef7b2e34b553cca0ac16e1356dee3ad0d8af0
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2021-06-13 22:29:45 +0300

    Added pagination on results page

---
+++ static/js/paginator.js
@@ -0,0 +1,56 @@
+var cnt = 3; //сколько отображаем сначала
+var cnt_page = 1; //кол-во страниц
+
+//выводим первые записи {cnt}
+var div_num = document.querySelectorAll(".num");
+if (div_num.length > cnt)
+    cnt_page = Math.ceil(div_num.length / cnt);
+console.log(cnt_page);
+
+for (var i = 0; i < div_num.length; i++) {
+  if (i < cnt) {
+    div_num[i].style.display = "block";
+  }
+  else {
+
+  }
+}
+
+//выводим список страниц
+var paginator = document.querySelector(".paginator");
+var page = "";
+for (var i = 0; i < cnt_page; i++) {
+  page += "<span data-page=" + i * cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
+}
+paginator.innerHTML = page;
+
+var main_page = document.getElementById("page1");
+main_page.classList.add("paginator_active");
+
+//листаем
+function pagination(event) {
+  var e = event || window.event;
+  var target = e.target;
+  var id = target.id;
+
+  if (target.tagName.toLowerCase() != "span") return;
+
+  var num_ = id.substr(4);
+  var data_page = +target.dataset.page;
+  main_page.classList.remove("paginator_active");
+  main_page = document.getElementById(id);
+  main_page.classList.add("paginator_active");
+
+  var j = 0;
+  for (var i = 0; i < div_num.length; i++) {
+    var data_num = div_num[i].dataset.num;
+    if (data_num <= data_page || data_num >= data_page)
+      div_num[i].style.display = "none";
+
+  }
+  for (var i = data_page; i < div_num.length; i++) {
+    if (j >= cnt) break;
+    div_num[i].style.display = "block";
+    j++;
+  }
+}
